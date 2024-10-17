b6ddb7d14b3b64e5727a441f924e819622d836ed
poshlikushat <161361012+poshlikushat@users.noreply.github.com>
2024-10-17 15:17:22 +0700

    Update quadratic_equation1.c

--- quadratic_equation1.c
+++ quadratic_equation1.c
@@ -5,7 +5,8 @@
 #define ERROR -1
 #define INFINIT_SOLUTION 10 
 #define NO_SOLUTIONS -10
-#define NO_SEN_COEF 3
+#define ONE_ROOT 1
+#define TWO_ROOTS 2
 
 int readCoefficient(char *prompt, double *value);
 double quadraticEquation(double a, double b, double c, double *root1_ptr, double *root2_ptr);
@@ -26,29 +27,27 @@ int main(void) {
         return ERROR;
     }
 
-    if (quadraticEquation(a, b, c, root1_ptr, root2_ptr) == INFINIT_SOLUTION)
+    double result = quadraticEquation(a, b, c, root1_ptr, root2_ptr);
+
+    if (result == INFINIT_SOLUTION)
     {
         printf("The equation has infinitly many soultions!\n");
     }
-    if (quadraticEquation(a, b, c, root1_ptr, root2_ptr) == NO_SOLUTIONS)
+    if (result == NO_SOLUTIONS)
     {
         printf("The equation has no avaliable soultions!\n");
     }
-    if (quadraticEquation(a, b, c, root1_ptr, root2_ptr) == NO_SEN_COEF)
-    {
-        printf("%lf\n", root1);
-    }
-    if (quadraticEquation(a, b, c, root1_ptr, root2_ptr) == 2) 
-    {
-        printf("%lf\n%lf\n", root1, root2);
-    }
-    if (quadraticEquation(a, b, c, root1_ptr, root2_ptr) == 1)
-    {
-        printf("%lf\n", root1);
-    }
-    if (quadraticEquation(a, b, c, root1_ptr, root2_ptr) == NO_REAL_NUMBERS) 
+    if (result == NO_REAL_NUMBERS)
     {
         printf("The equation has no real roots!\n");
+    }
+    if (result == ONE_ROOT)
+    {
+        printf("%lf\n", root1);
+    }
+    if (result == TWO_ROOTS)
+    {
+        printf("%lf\n%lf\n", root1, root2);
     }
 }
 
@@ -82,17 +81,17 @@ double quadraticEquation(double a, double b, double c, double *root1_ptr, double
         else
         {
             *root1_ptr = -c / b;
-            return NO_SEN_COEF;
+            return ONE_ROOT;
         }
     }
 
     if (discriminant > 0) {
         *root1_ptr = (-b + sqrt(discriminant)) / (2 * a);
         *root2_ptr = (-b - sqrt(discriminant)) / (2 * a);
-        return 2;
+        return TWO_ROOTS;
     } else if (discriminant == 0) {
         *root1_ptr = -b / (2 * a);
-        return 1;
+        return ONE_ROOT;
     } else {
         return NO_REAL_NUMBERS;
     }
