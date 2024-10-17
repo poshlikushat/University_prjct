14bb30195e6d36d514d623a2b4f5d71dca4b5834
poshlikushat <161361012+poshlikushat@users.noreply.github.com>
2024-10-17 15:01:33 +0700

    Update quadratic_equation1.c

--- quadratic_equation1.c
+++ quadratic_equation1.c
@@ -1,13 +1,20 @@
 #include <stdio.h>
 #include <math.h>
 
-#define ERROR 1
+#define NO_REAL_NUMBERS -2
+#define ERROR -1
+#define INFINIT_SOLUTION 10 
+#define NO_SOLUTIONS -10
+#define NO_SEN_COEF 3
 
 int readCoefficient(char *prompt, double *value);
-double quadraticEquation(double a, double b, double c);
+double quadraticEquation(double a, double b, double c, double *root1_ptr, double *root2_ptr);
 
 int main(void) {
     double a, b, c;
+    double root1, root2;
+    double* root1_ptr = &root1;
+    double* root2_ptr = &root2;
 
     if (readCoefficient("Input a senior coefficient: ", &a)) {
         return ERROR;
@@ -19,9 +26,31 @@ int main(void) {
         return ERROR;
     }
 
-    return quadraticEquation(a, b, c);
+    if (quadraticEquation(a, b, c, root1_ptr, root2_ptr) == INFINIT_SOLUTION)
+    {
+        printf("The equation has infinitly many soultions!\n");
+    }
+    if (quadraticEquation(a, b, c, root1_ptr, root2_ptr) == NO_SOLUTIONS)
+    {
+        printf("The equation has no avaliable soultions!\n");
+    }
+    if (quadraticEquation(a, b, c, root1_ptr, root2_ptr) == NO_SEN_COEF)
+    {
+        printf("%lf\n", root1);
+    }
+    if (quadraticEquation(a, b, c, root1_ptr, root2_ptr) == 2) 
+    {
+        printf("%lf\n%lf\n", root1, root2);
+    }
+    if (quadraticEquation(a, b, c, root1_ptr, root2_ptr) == 1)
+    {
+        printf("%lf\n", root1);
+    }
+    if (quadraticEquation(a, b, c, root1_ptr, root2_ptr) == NO_REAL_NUMBERS) 
+    {
+        printf("The equation has no real roots!\n");
+    }
 }
-
 
 int readCoefficient(char *prompt, double *value) {
     printf("%s", prompt);
@@ -34,34 +63,37 @@ int readCoefficient(char *prompt, double *value) {
     return 0;
 }
 
-
-double quadraticEquation(double a, double b, double c) {
+double quadraticEquation(double a, double b, double c, double *root1_ptr, double *root2_ptr) {
     double discriminant = b * b - 4 * a * c;
 
-    if (a == 0) {
-        if (b == 0) {
-            if (c == 0) {
-                printf("The equation has infinitely many solutions!\n");
-            } else {
-                printf("No available solutions for the equation!\n");
+    if (a == 0)
+    {
+        if (b == 0)
+        {
+            if (c == 0)
+            {
+                return INFINIT_SOLUTION;
             }
-        } else {
-            printf("The solution is: %.2lf\n", -c / b);
+            else
+            {
+                return NO_SOLUTIONS;
+            }
         }
-        return 0;
+        else
+        {
+            *root1_ptr = -c / b;
+            return NO_SEN_COEF;
+        }
     }
 
     if (discriminant > 0) {
-        printf("Root 1: %.2lf\n", (-b + sqrt(discriminant)) / (2 * a));
-        printf("Root 2: %.2lf\n", (-b - sqrt(discriminant)) / (2 * a));
+        *root1_ptr = (-b + sqrt(discriminant)) / (2 * a);
+        *root2_ptr = (-b - sqrt(discriminant)) / (2 * a);
+        return 2;
     } else if (discriminant == 0) {
-        printf("The equation has a real root: %.2lf\n", -b / (2 * a));
+        *root1_ptr = -b / (2 * a);
+        return 1;
     } else {
-        double real_part = -b / (2 * a);
-        double imaginary_part = sqrt(fabs(discriminant)) / (2 * a);
-        printf("Root 1: %.2lf - %.2lfi\n", real_part, imaginary_part);
-        printf("Root 2: %.2lf + %.2lfi\n", real_part, imaginary_part);
+        return NO_REAL_NUMBERS;
     }
-
-    return 0;
 }
