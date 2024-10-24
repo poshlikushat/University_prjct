0d1c7186bc8655a9397b25a557ae2e98dc15d785
poshlikushat <161361012+poshlikushat@users.noreply.github.com>
2024-10-24 20:10:25 +0700

    вернул mergeSort

--- mergeSort.c
+++ mergeSort.c
@@ -1,14 +1,17 @@
 #include "mergeSort.h"
 
-static void mergeSortRecursion(int* arr, int l, int r);
-static void mergeSortedArrays(int* arr, int l, int m, int r);
+static void mergeSortRecursion(int *arr, int l, int r);
+static void mergeSortedArrays(int *arr, int l, int m, int r);
 
-void mergeSort(int* arr, int length) {
+void mergeSort(int *arr, int length)
+{
 	mergeSortRecursion(arr, 0, length - 1);
 }
 
-static void mergeSortRecursion (int* arr, int l, int r) {
-	if (l < r) {
+static void mergeSortRecursion(int *arr, int l, int r)
+{
+	if (l < r)
+	{
 		int m = l + (r - l) / 2;
 
 		mergeSortRecursion(arr, l, m);
@@ -17,8 +20,8 @@ static void mergeSortRecursion (int* arr, int l, int r) {
 	}
 }
 
-
-static void mergeSortedArrays (int* arr, int l, int m, int r) {
+static void mergeSortedArrays(int *arr, int l, int m, int r)
+{
 	int leftLength = m - l + 1;
 	int rightLength = r - m;
 
@@ -27,11 +30,13 @@ static void mergeSortedArrays (int* arr, int l, int m, int r) {
 
 	int i, j, k;
 
-	for (i = 0; i < leftLength; i++) {
+	for (i = 0; i < leftLength; i++)
+	{
 		tempLeft[i] = arr[l + i];
 	}
 
-	for (j = 0; j < rightLength; j++) {
+	for (j = 0; j < rightLength; j++)
+	{
 		tempRight[j] = arr[m + 1 + j];
 	}
 
@@ -61,28 +66,10 @@ static void mergeSortedArrays (int* arr, int l, int m, int r) {
 		k++;
 	}
 
-
 	while (j < rightLength)
 	{
 		arr[k] = tempRight[j];
 		j++;
 		k++;
 	}
-}
-
-
-int compareInt (void* ptr_a, void* ptr_b) {
-	
-}
-
-int compareFloat(void *ptr_a, void *ptr_b) {
-
-}
-
-int compareChar(void *ptr_a, void *ptr_b) {
-
-}
-
-int compareString(void *ptr_a, void *ptr_b) { 
-
 }
--- mergeSortMain.c
+++ mergeSortMain.c
@@ -1,17 +1,27 @@
 #include <stdio.h>
 #include "mergeSort.h"
 
+
+int compareInt(void *ptr_a, void *ptr_b);
+
+
 int main() {
 	int array[] = {9, 4, 8, 1, 7, 0, 3, 2, 5, 6};
 	int length = sizeof(array) / sizeof(array[0]);
 
-	mergeSort(array, length);
+	void* arr_ptr = (void*)array;
+	void* ptr0 = (void*)((char*)arr_ptr + 0);
+	void* ptr1 = (void*)((char*)arr_ptr + 4);
 
-	for (int i = 0; i < length; i++) {
-		printf("%d ", array[i]);
+	printf("%d\n", compareInt(ptr0, ptr1));
+
+}
+
+
+int compareInt(void *ptr_a, void *ptr_b)
+{
+	if (*(int *)(ptr_a) >= *(int *)(ptr_b)) {
+		return 1;
 	}
-	printf("\n");
-
-	return 0;
-
+	else return 0;
 }
